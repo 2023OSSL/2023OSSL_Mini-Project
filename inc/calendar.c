f396a0c9fb2ace70a77b6b70082da06bcc3b3262
JiseongSon381 <sonjisung901@handong.ac.kr>
2023-05-17 23:16:42 +0900

    달력 출력에 관한 함수 추가

--- inc/calendar.c
+++ inc/calendar.c
@@ -1,3 +1,21 @@
 // 달력 출력, 날짜에 관한 계산 등에 필요한 함수들을 작성
 #include "calendar.h"
 
+// 달력 출력
+void DisplayCalendar()
+{
+
+}
+
+// 입력 달 시작 요일 계산
+int getStartDay(int year, int month)
+{
+    return 0;    
+}
+
+// 윤년 계산
+int isLeapYear(int year)
+{
+    if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) return 1;
+    else return 0;
+}
--- inc/calendar.h
+++ inc/calendar.h
@@ -1,1 +1,6 @@
 #include "CRUD_calendar.h"
+
+
+void DisplayCalendar();
+int getStartDay(int year, int month);
+int isLeapYear(int year);
